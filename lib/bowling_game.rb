844074c1f6f37ac46f0d2c1cff6deacf92e35d94
Alex Key <alex@reservationgenie.com>
2020-11-24 19:35:32 -0600

    green

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,13 +1,32 @@
 class BowlingGame
 
   attr_reader :rolls
+  attr_reader :first_in_frame
+
+  def initialize
+    @first_in_frame = 0
+  end
 
   def rolls(rolls)
     @rolls = rolls
   end
 
   def score
-    @rolls.inject(:+)
+    frame = 0
+    score = 0
+
+    while frame < 10
+      if @rolls[@first_in_frame] + @rolls[@first_in_frame + 1] == 10
+        score += 10 + @rolls[@first_in_frame + 2]
+      else
+        score += @rolls[@first_in_frame] + @rolls[@first_in_frame + 1]
+      end
+
+      @first_in_frame += 2
+      frame += 1
+    end
+
+    score
   end
 
 end
