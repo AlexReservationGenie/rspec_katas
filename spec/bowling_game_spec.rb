cc4f95e30566f75826295a701fb56572b3069cbb
Alex Key <alex@reservationgenie.com>
2020-11-24 19:25:02 -0600

    Updated spec - red

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,9 +1,16 @@
 require_relative '../lib/bowling_game'
 
 describe BowlingGame do
+
   let(:game) { BowlingGame.new }
+
   it "scores a gutter game" do
     game.rolls([0] * 20)
     expect(game.score).to eq(0)
+  end
+
+  it "scores a spare" do
+    game.rolls([4,6,5] + [1]*17)
+    expect(game.score).to eq(37)
   end
 end
